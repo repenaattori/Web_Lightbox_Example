476879188bee52619a0312d06064fb34b7e5233e
repenaattori <reimarii@oamk.fi>
2022-12-07 08:52:32 +0200

    init

---
+++ code.js
@@ -0,0 +1,8 @@
+let images = [
+    "https://picsum.photos/id/1/800",
+    "https://picsum.photos/id/20/800",
+    "https://picsum.photos/id/45/800",
+    "https://picsum.photos/id/80/800",
+    "https://picsum.photos/id/23/800",
+]
+
